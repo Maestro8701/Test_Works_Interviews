41dc9ebf8f768b76ac0e67053773a07382710480
Maestro8701 <149194877+Maestro8701@users.noreply.github.com>
2025-10-02 22:11:44 +0700

    Create PersistenceService.h

---
+++ Lora/src/core/storage/PersistenceService.h
@@ -0,0 +1,16 @@
+//
+// Created by never on 17.05.2025.
+//
+
+#ifndef PERSISTENCESERVICE_H
+#define PERSISTENCESERVICE_H
+
+
+
+class PersistenceService {
+
+};
+
+
+
+#endif //PERSISTENCESERVICE_H
